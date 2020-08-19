5600deed52cad75cdf6f84ebfb890a1c172a1644
Oriol Garrobé <oriol.garrobe@gmail.com>
2020-08-19 15:50:49 +0200

    F1 for Adaboost and RF

--- dev/random_forest.R
+++ dev/random_forest.R
@@ -3,40 +3,12 @@ load("dev/anal.Rda")
 summary(anal)
 
 
-
-
 ####### RANDOM FOREST AND ADABOOST #######
 
 # Libraries
 library(mboost)
 library(randomForest)
 
-# Data modelling
-data <- anal
-data$goal <- as.factor(data$goal)
-data$id <- NULL
-
-#Divide data
-n=dim(data)[1]
-
-set.seed(12345)
-id=sample(1:n, floor(n*(2/3)))
-train=data[id,]
-id1=setdiff(1:n, id)
-set.seed(12345)
-id2=sample(id1, floor(n*(1/3)))
-test=data[id2,]
-
-# Fit the random forest model
-me.random <- rep(0,10)
-for (e in n.trees) {
-  random <- randomForest(formula = goal~., data = train, ntree=e)
-  random.pred <- predict(random, newdata = test, type = "class")
-  cm.random <- table(random.pred, test$goal)
-  me.random[which(e==n.trees)] <- (cm.random[2,1]+cm.random[1,2])/(nrow(test))
-}
-
-# Fit the Adaboost model
 
 # Set logical values to numeric in order to avoid issues
 data <- anal
@@ -49,18 +21,25 @@ data$home <- as.numeric(data$home)
 data$pressure_block <- as.numeric(data$pressure_block)
 data$gk_obstacle <- as.numeric(data$gk_obstacle)
 
-#Divide data
-n=dim(data)[1]
+### divide data into training - validation - test sets (while balancing the 2 classes = stratified sampling)
+prop.table(table(data$goal)) # 16.241% goal
+train_index = caret::createDataPartition(data$goal, p = 0.7, list = F) # 70% training
+prop.table(table(data$goal[train_index])) # 16.333% goal
 
-set.seed(12345)
-id=sample(1:n, floor(n*(2/3)))
-train=data[id,]
-id1=setdiff(1:n, id)
-set.seed(12345)
-id2=sample(id1, floor(n*(1/3)))
-test=data[id2,]
+train = data[train_index,]
+test = data[-train_index,]
 
 
+# Fit the random forest model
+me.random <- rep(0,10)
+for (e in n.trees) {
+  random <- randomForest(formula = goal~., data = train, ntree=e)
+  random.pred <- predict(random, newdata = test, type = "class")
+  cm.random <- table(random.pred, test$goal)
+  me.random[which(e==n.trees)] <- (cm.random[2,1]+cm.random[1,2])/(nrow(test))
+}
+
+# Fit the Adaboost model
 n.trees <- seq(from = 10, to = 100, by = 10)
 me.ada <- rep(0,10)
 for (i in n.trees) {
@@ -78,6 +57,37 @@ plot(n.trees, me.ada, ylim = c(0,0.30), pch = 16, col = "red",
      main = "Adaboost Vs. Random forest")
 legend("topright",c("Adaboost","R. Forest"),fill=c("red","blue"))
 points(n.trees, me.random, pch = 16, col = "blue")
+
+# Final models
+
+# Best model Random Forest with 40 trees
+best_random_forest <- randomForest(formula = goal~., data = train, ntree=40)
+predicted_random_forest <- predict(best_random_forest, newdata = test, type = "class")
+cm_random_forest <- table(test$goal, predicted_random_forest)
+
+# Best model AdaBoost with 30 trees
+best_adaboost <- blackboost(formula = goal~., data = train, family = AdaExp(), control = boost_control(mstop = 30))
+predicted_adaboost <- predict(best_adaboost, newdata = test, type = "class")
+cm_adaboost <- table(test$goal, predicted_adaboost)
+
+# Computing Precision / Recall / F1-Score
+
+# Random forest
+recall_random_forest = cm_random_forest[2,2] / (cm_random_forest[2,2] + cm_random_forest[2,1])
+recall_random_forest
+precision_random_forest = cm_random_forest[2,2] / (cm_random_forest[2,2] + cm_random_forest[1,2])
+precision_random_forest
+f1_random_forest = (2 * recall_random_forest * precision_random_forest) / (recall_random_forest + precision_random_forest)
+f1_random_forest
+
+# AdaBoost
+recall_adaboost = cm_adaboost[2,2] / (cm_adaboost[2,2] + cm_adaboost[2,1])
+recall_adaboost
+precision_adaboost = cm_adaboost[2,2] / (cm_adaboost[2,2] + cm_adaboost[1,2])
+precision_adaboost
+f1_adaboost = (2 * recall_adaboost * precision_adaboost) / (recall_adaboost + precision_adaboost)
+f1_adaboost
+
 
 
 ##########################################
